c8b3b0f72418501afa43568a1982fd3ab3254a25
Rohit Shetty <ht68rohit@gmail.com>
2019-03-11 14:31:19 +0530

    updated test case

--- messaging/message_test.go
+++ messaging/message_test.go
@@ -3,21 +3,21 @@ package messaging
 import (
 	"bytes"
 	"encoding/json"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 	"net/http"
-	"github.com/tbalthazar/onesignal-go"
 	"net/http/httptest"
 	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/tbalthazar/onesignal-go"
 )
 
 var _ = Describe("List Applications", func() {
 
-
 	os.Setenv("USER_KEY", "NjY1M2ViODktNTIxMi00ODBmLTk4Y2YtMjE3YzE1Y2I0ZmJl")
 	os.Setenv("APP_KEY", "MDdjMDJlY2ItNTVhOC00NDI1LTllOTEtOTdkNzgyNWUzYjIz")
 
-	request, err := http.NewRequest("GET", "/send",nil)
+	request, err := http.NewRequest("GET", "/send", nil)
 	if err != nil {
 	}
 	recorder := httptest.NewRecorder()
@@ -33,22 +33,17 @@ var _ = Describe("List Applications", func() {
 	})
 })
 
-
-
 var _ = Describe("OneSignal messaging", func() {
 	var argumentData onesignal.NotificationRequest
 	argumentData.AppID = "aebc588d-f5a5-4668-8777-0debf272783a"
 	argumentData.Headings = map[string]string{"en": "Test Notification"}
-	argumentData.Contents =  map[string]string{"en": "Demo testing"}
-	argumentData.IsAnyWeb =  true
-	argumentData.IncludePlayerIDs = []string{"7f3ec4c5-9aef-4eb0-8290-ad0d2eaf9c9a","4b41af62-6bbb-4881-be2d-4f969d5be88d","c94168ed-8f7f-480a-9a1e-a445f9e0ae42"}
+	argumentData.Contents = map[string]string{"en": "Demo testing"}
+	argumentData.IsAnyWeb = true
+	argumentData.IncludePlayerIDs = []string{"7f3ec4c5-9aef-4eb0-8290-ad0d2eaf9c9a", "4b41af62-6bbb-4881-be2d-4f969d5be88d", "c94168ed-8f7f-480a-9a1e-a445f9e0ae42"}
 	argumentData.SmallIcon = "/my-icon.png"
 
 	requestBody := new(bytes.Buffer)
 	json.NewEncoder(requestBody).Encode(argumentData)
-
-	// os.Setenv("USER_KEY", "NjY1M2ViODktNTIxMi00ODBmLTk4Y2YtMjE3YzE1Y2I0ZmJl")
-	// os.Setenv("APP_KEY", "MDdjMDJlY2ItNTVhOC00NDI1LTllOTEtOTdkNzgyNWUzYjIz")
 
 	request, err := http.NewRequest("POST", "/send", requestBody)
 	if err != nil {
@@ -65,7 +60,3 @@ var _ = Describe("OneSignal messaging", func() {
 		})
 	})
 })
-
-
-
-
